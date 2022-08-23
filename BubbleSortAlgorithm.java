210468541b8a9b6ab73b3480e0e46cff4eb834c1
Enes Catal <93399183+en3sctl@users.noreply.github.com>
2022-08-23 17:32:48 +0300

    Add files via upload

---
+++ BubbleSortAlgorithm.java
@@ -0,0 +1,29 @@
+package DataStructure;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int lastSortedIndex = intArray.length - 1; lastSortedIndex > 0; lastSortedIndex--) {
+            for (int i = 0; i < lastSortedIndex; i++) {
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i]);
+            System.out.print(" ");
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
